8c0c87912db261b9c3a9cc7ae1d2751e9cb2dd76
Travis Haynes <travis.j.haynes@gmail.com>
2015-03-04 20:42:00 -0800

    documented inflections and updated the way they're loaded

--- lib/evoke/inflections.rb
+++ lib/evoke/inflections.rb
@@ -1,7 +1,11 @@
-module Evoke::Inflections; end
+module Evoke
+  # Inflections define new methods on the String class to transform names for
+  # different purposes.
+  module Inflections
+    # Load the inflections.
+    Dir[File.expand_path("../inflections/*.rb", __FILE__)].each {|f| require f }
 
-require 'evoke/inflections/camelize'
-require 'evoke/inflections/underscore'
-
-String.send(:include, Evoke::Inflections::Camelize)
-String.send(:include, Evoke::Inflections::Underscore)
+    # Add the inflections to the String class.
+    constants.each {|i| String.send(:include, const_get(i)) }
+  end
+end
